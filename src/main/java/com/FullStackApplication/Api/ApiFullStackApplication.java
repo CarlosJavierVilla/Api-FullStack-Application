db0271d84692de1adcadc7ea8a947a36726c8470
javier <villarestrepocarlos@gmail.com>
2023-03-07 09:30:32 +0100

    Firs commit

---
+++ src/main/java/com/FullStackApplication/Api/ApiFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.FullStackApplication.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FullStackApplication/Api/ApiFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FullStackApplication.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
