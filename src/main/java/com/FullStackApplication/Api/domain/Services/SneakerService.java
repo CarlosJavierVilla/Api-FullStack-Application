1602aa6bd3390387907d698a17d5256006cb657e
marcygon <marciasofia49@gmail.es>
2023-03-27 16:15:02 +0200

    defaultImgUrlValidator

--- src/main/java/com/FullStackApplication/Api/domain/Services/SneakerService.java
+++ src/main/java/com/FullStackApplication/Api/domain/Services/SneakerService.java
@@ -1,11 +1,13 @@
 package com.FullStackApplication.Api.domain.Services;
 
 import com.FullStackApplication.Api.domain.Dtos.SneakerRequest;
-import com.FullStackApplication.Api.domain.Models.Category;
 import com.FullStackApplication.Api.domain.Models.Sneaker;
 import com.FullStackApplication.Api.infrastructure.Repositories.ICategoryRepository;
 import com.FullStackApplication.Api.infrastructure.Repositories.ISneakerRepository;
 import org.springframework.stereotype.Service;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import java.util.List;
 
@@ -14,6 +16,7 @@ public class SneakerService {
     private final ISneakerRepository sneakerRepository;
     private final AdminService adminService;
     private final ICategoryRepository categoryRepository;
+
 
 
     public SneakerService(ISneakerRepository sneakerRepository, AdminService adminService, ICategoryRepository categoryRepository) {
@@ -45,21 +48,43 @@ public class SneakerService {
         return sneakerRepository.findByCategory_Id(2L);
     }
 
+
+
+    public static boolean validateURL(String urlString){
+        try{
+            new URL(urlString).toURI();
+        }
+        catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+
     public Sneaker addSneaker(SneakerRequest request) {
         var category = categoryRepository.findById(request.getCategoryId())
                 .orElseThrow(() -> new RuntimeException("This category doesn't exist. Please try again."));
         var admin = this.adminService.getAdminRegistered();
         var sneaker = new Sneaker();
+        var urlDefault = "https://static.thenounproject.com/png/1554489-200.png";
+
         sneaker.setBrand(request.getBrand());
         sneaker.setModel(request.getModel());
         sneaker.setPrice(request.getPrice());
         sneaker.setImg(request.getImg());
+
+        if(validateURL(request.getImg()) == false){
+            sneaker.setImg(urlDefault);
+        }
+
         sneaker.setDescription(request.getDescription());
         sneaker.setHighlights(request.getHighlights());
         sneaker.setCategory(category);
         sneaker.setAdmin(admin);
         return this.sneakerRepository.save(sneaker);
     }
+
+
     public void deleteById(Long sneakerId) {
         var admin = this.adminService.getAdminRegistered();
         var sneaker = sneakerRepository.findById(sneakerId).get();
