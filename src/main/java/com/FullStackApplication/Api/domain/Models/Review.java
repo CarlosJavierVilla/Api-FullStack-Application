f7e4c5239b4e7bfe00d4c397346d6747a119b702
marcygon <marciasofia49@gmail.es>
2023-04-04 15:26:22 +0200

    userModel

---
+++ src/main/java/com/FullStackApplication/Api/domain/Models/Review.java
@@ -0,0 +1,9 @@
+package com.FullStackApplication.Api.domain.Models;
+
+public class Review {
+    //id de la review
+    //necesita el id de un user
+    //necesita el id de una sneaker
+    //un texto
+    //una valoracion del 1 al 5 estrellas (extra)
+}
